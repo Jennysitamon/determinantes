50908ab16f9b419e6b0e8ba1b4886340c0e20eb3
jeny <jenymontiel13@gmail.com>
2023-11-21 21:59:02 -0600

    DeterminanteOficial

--- src/determinantes4b/Determinantes4b.java
+++ src/determinantes4b/Determinantes4b.java
@@ -6,82 +6,65 @@ import java.io.InputStreamReader;
 
 public class Determinantes4b {
 
-    public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int opcion;
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        do {
-            System.out.println("Hola😍. Elige la opción de lo que deseas realizar:");
-            System.out.println("1. Calcular determinante matriz");
-            System.out.println("2. Salir");
-            System.out.print("Opción: ");
-
-            try {
-                opcion = Integer.parseInt(reader.readLine());
-            } catch (IOException | NumberFormatException e) {
-                System.out.println("Inténtalo de nuevo :(");
-                opcion = -1; 
-                continue;
+    static void obtenerCofactor(int matriz[][], int temp[][], int p, int q, int n) {
+        int i = 0, j = 0;
+        for (int fila = 0; fila < n; fila++) {
+            for (int columna = 0; columna < n; columna++) {
+                if (fila != p && columna != q) {
+                    temp[i][j++] = matriz[fila][columna];
+                    if (j == n - 1) {
+                        j = 0;
+                        i++;
+                    }
+                }
             }
-
-            switch (opcion) {
-                case 1 -> calcularMatriz(reader);
-                case 2 -> System.out.println("Saliendo del programa...😣");
-                default -> System.out.println("Inténtalo de nuevo :(");
-            }
-        } while (opcion != 0);
-
-        try {
-            reader.close();
-        } catch (IOException e) {
         }
     }
 
-    public static void calcularMatriz(BufferedReader reader) {
-        try {
-            System.out.print("Número de filas de la matriz: ");
-            int filas = Integer.parseInt(reader.readLine());
-
-            System.out.print("Número de columnas de la matriz: ");
-            int columnas = Integer.parseInt(reader.readLine());
-
-            if (filas != columnas) {
-                System.out.println("No es una matriz cuadrada. El número de filas debe ser igual al número de columnas.");
-                return;
-            }
-
-            int[][] matriz = new int[filas][columnas];
-      
-            for (int i = 0; i < filas; i++) {
-                for (int j = 0; j < columnas; j++) {
-                    System.out.print("Ingrese el elemento en la posición [" + (i + 1) + "][" + (j + 1) + "]: ");
-                    matriz[i][j] = Integer.parseInt(reader.readLine());
-                }
-            }
-
-            
-            System.out.println("Matriz ingresada:");
-            for (int i = 0; i < filas; i++) {
-                for (int j = 0; j < columnas; j++) {
-                    System.out.print(matriz[i][j] + "\t");
-                }
-                System.out.println();
-            }
-
-            int suma = calcularSumaMatriz(matriz);
-            System.out.println("Resultado de la suma de la matriz: " + suma);
-
-        } catch (IOException | NumberFormatException e) {
-            System.out.println("Inténtalo de nuevo, error al leer datos :(");
+    static int determinanteMatriz(int matriz[][], int n) {
+        int determinante = 0;
+        if (n == 1) {
+            return matriz[0][0];
         }
+        int temp[][] = new int[n][n];
+        int multiplicador = 1;
+        for (int f = 0; f < n; f++) {
+            obtenerCofactor(matriz, temp, 0, f, n);
+            determinante += multiplicador * matriz[0][f] * determinanteMatriz(temp, n - 1);
+            multiplicador = -multiplicador;
+        }
+        return determinante;
     }
-    public static int calcularSumaMatriz(int[][] matriz) {
-        int suma = 0;
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Ingresa el tamaño de la matriz:\t");
+        int n = Integer.parseInt(reader.readLine());
+        int matriz[][] = new int[n][n];
+        System.out.println("Inserta los valores:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("mat[" + i + "][" + j + "]:\t");
+                matriz[i][j] = Integer.parseInt(reader.readLine());
+            }
+        }
+
+        System.out.println("\nMatriz ingresada:");
+        imprimirMatriz(matriz);
+
+        System.out.println("\nLa determinante de la matriz es:\t" + determinanteMatriz(matriz, n));
+
+        reader.close();
+    }
+
+    static void imprimirMatriz(int matriz[][]) {
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                suma += matriz[i][j];
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
             }
+            System.out.println();
         }
-        return suma;
     }
 }
